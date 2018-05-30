bc9b4f375ec933ef1d8f28010e3796958ea9db8e
EC2 Default User <ec2-user@ip-172-31-33-157.us-east-2.compute.internal>
2018-05-30 00:37:13 +0000

    validation

--- database/migrations/2018_05_29_163528_add_title_to_messages_table.php
+++ database/migrations/2018_05_29_163528_add_title_to_messages_table.php
@@ -14,7 +14,7 @@ class AddTitleToMessagesTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            //
+            $tasks->string()
         });
     }
 
